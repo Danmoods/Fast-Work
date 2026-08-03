a66d49f090ff4467176673dff8814e8ab79b63b5
Danmoods <saitamang1234@gmail.com>
2026-08-03 04:38:56 +0300

    Set up flask extensions and application config properly

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask
-from extensions import SQLAlchemy
-from flask_migrate import Migrate
+
+from extensions import db, ma, jwt, migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jobs.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = "your-secret-key"
+
+
+db.init_app(app)
+ma.init_app(app)
+jwt.init_app(app)
+migrate.init_app(app, db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
