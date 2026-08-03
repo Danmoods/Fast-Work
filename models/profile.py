6aefc88c94523f7382f31b49cb1abf09e4095f0a
Danmoods <saitamang1234@gmail.com>
2026-08-03 04:56:50 +0300

    Created User and Profile models with one-one relationship

--- models/profile.py
+++ models/profile.py
@@ -2,12 +2,9 @@ class Profile(db.Model):
     __tablename__ = "profiles"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer,
-                        db.ForeignKey("users.id"))
-
-    phone = db.Column(db.String(20))
-    bio = db.Column(db.Text)
-
-    user = db.relationship(
-        "User",back_populates="profile"
-    )
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    phone = db.Column(db.String(20),nullable=False)
+    bio = db.Column(db.Text,nullable=True)
+    location = db.Column(db.String(100),nullable=True)
+    profile_photo = db.Column(db.String(200),nullable=True)
+    user = db.relationship("User",back_populates="profile")
--- models/user.py
+++ models/user.py
@@ -1,6 +1,15 @@
 from extensions import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100),unique=True,nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # 'employer' or 'worker'
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    profile = db.relationship("Profile", back_populates="user", uselist=False)
+    
