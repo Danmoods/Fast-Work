b773d61e417594ac53bb682e7ac3ce8323ee5408
Danmoods <saitamang1234@gmail.com>
2026-08-03 05:34:40 +0300

    Introduced config.py and renamed worker_skills and skills

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///jobs.db"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.getenv(
+        "JWT_SECRET_KEY",
+        "super-secret-key"
+    )
